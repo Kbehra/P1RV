86dbf58c7923b2688f1e45d73df5f6c40813ac3b
alicia <alicia82@orange.fr>
2019-12-05 20:24:07 +0100

    Ajout d'une protection avant de fermet une des sous-fenetre + view affichage ou non de la fenetre + help : a mettre les touches utilisee

--- src/mymainwindow.cpp
+++ src/mymainwindow.cpp
@@ -14,32 +14,31 @@ MyMainWindow::MyMainWindow() {
     setWindowTitle("HeightMap Creator Tool");
 
     //Permet d'avoir plusieurs fenetre dans notre fenetre principale
-    QMdiArea *zoneCentrale = new QMdiArea;
+    zoneCentrale = new QMdiArea;
 
     //On creer 2 fenetres
     fenetre_opengl = new MyWindow(this);
     QTextEdit *zoneTexte2 = new QTextEdit("Drawing Heightmap");
 
-    QMdiSubWindow *dessin = zoneCentrale->addSubWindow(zoneTexte2); //second window
-    QMdiSubWindow *rendu_3d = zoneCentrale->addSubWindow(fenetre_opengl); //first window
+    //idealement on creer une fenetre a part qui viens se mettre dans la subwindow pour dessin
+    dessin = zoneCentrale->addSubWindow(zoneTexte2); //second window
+    rendu_3d = zoneCentrale->addSubWindow(fenetre_opengl); //first window
 
     zoneCentrale->tileSubWindows();
     setCentralWidget(zoneCentrale);
 
     //Onglet File
-    QMenu *menuFichier = menuBar()->addMenu("&File");
-
+    menuFichier = menuBar()->addMenu("&File");
     //sous-menu File
-    QAction *chargerMap = new QAction("&Load Map...", fenetre_opengl);
+    chargerMap = new QAction("&Load Map...", fenetre_opengl);
     chargerMap->setShortcut(QKeySequence("Ctrl+M"));
-    QAction *chargerTex = new QAction("&Load Texture...", fenetre_opengl);
+    chargerTex = new QAction("&Load Texture...", fenetre_opengl);
     chargerTex->setShortcut(QKeySequence("Ctrl+T"));
-    QAction *exportFile = new QAction("&Export (.stl)", fenetre_opengl);
+    exportFile = new QAction("&Export (.stl)", fenetre_opengl);
     exportFile->setShortcut(QKeySequence("Ctrl+S"));
-    QAction *config = new QAction("&Settings", this); //options de langues, couleur fenetre...
-    QAction *actionQuitter = new QAction("&Quit", this);
+    config = new QAction("&Settings", this); //options de langues, couleur fenetre...
+    actionQuitter = new QAction("&Quit", this);
     actionQuitter->setShortcut(QKeySequence("Ctrl+Q"));
-
     //ajout a l'onglet File
     menuFichier->addAction(chargerMap);
     menuFichier->addAction(chargerTex);
@@ -48,12 +47,31 @@ MyMainWindow::MyMainWindow() {
     menuFichier->addAction(actionQuitter);
 
     //Onglet Edit
-    QMenu *menuEdition = menuBar()->addMenu("&Edit");
+    menuEdition = menuBar()->addMenu("&Edit");
     //sous-menu Edit
-    QAction *params = new QAction("&Parameters",fenetre_opengl); //parametre modifiable
+    params = new QAction("&Parameters",fenetre_opengl); //parametre modifiable
     //ajout a l'onglet Edit
     menuEdition->addAction(params);
 
+    //Onglet View
+    menuAffichage = menuBar()->addMenu("&View");
+    //sous-menu Edit
+    openHeightMap = new QAction("&Heightmap",rendu_3d);
+    openHeightMap->setCheckable(true);
+    openHeightMap->setChecked(true);
+    openDraw = new QAction("&Draw",dessin);
+    openDraw->setCheckable(true);
+    openDraw->setChecked(true);
+    //ajout a l'onglet Edit
+    menuAffichage->addAction(openHeightMap);
+    menuAffichage->addAction(openDraw);
+
+    //Onglet Help
+    menuAide = menuBar()->addMenu("&Help");
+    //sous-menu Help
+    about = new QAction(tr("&About.."), this);
+    //ajout a l'onglet Help
+    menuAide->addAction(about);
 
     //Connects
     connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
@@ -62,14 +80,39 @@ MyMainWindow::MyMainWindow() {
     connect(exportFile, SIGNAL(triggered()), fenetre_opengl, SLOT(saveFile()));
     //connect(config, SIGNAL(triggered()), this, SLOT());
     connect(params, SIGNAL(triggered()), fenetre_opengl,SLOT(chooseParam()));
-
+    connect(openHeightMap, SIGNAL(triggered()), this,SLOT(viewWindow()));
+    connect(openDraw, SIGNAL(triggered()), this,SLOT(viewWindow()));
+    connect(about, SIGNAL(triggered()), this, SLOT(aboutApp()));
 }
 //TODO dans le menu Edit, ajouter la possibilite à l'utilisateur de réouvrir une fenetre fermée (ex: visualisation de l'image)
 // ou Fenetre OpenGL
 
 
 //SLOT
+void MyMainWindow::aboutApp()
+{
+    QMessageBox::about(this, "A propos de Application",
+                       "<b>Application</b> permet de creer des Heightmaps "
+                          "personnalisee avec differents parametre. Il est possible "
+                          "de sauvegarder le rendu 3D au format (.stl)."
+                          "Explication des raccourcis:");
+}
+
 void MyMainWindow::setConfig(){
     //TODO mettre le changement de langue ou autre
 
+}
+
+void MyMainWindow::viewWindow(){
+
+   if(openHeightMap->isChecked()){
+       rendu_3d->show();
+   } else {
+       rendu_3d->hide();
+   }
+    if(openDraw->isChecked()){
+        dessin->show();
+    } else {
+        dessin->hide();
+    }
 }
